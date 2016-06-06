4e8352e23f1b1ca1f3582328c57513d46dca896d
Akshay <Akshay>
2016-06-07 00:06:48 +0530

    Updated project

---
+++ Assets/Scripts/Introstart.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Introstart : MonoBehaviour 
+{
+	public GameObject book;
+
+	public void OnMouseEnter()
+	{
+		gameObject.GetComponent<Renderer> ().material.color = Color.green;
+	}
+
+	public void OnMouseUp()
+	{
+		book.GetComponent<Animator> ().enabled = true;
+		gameObject.SetActive (false);
+	}
+
+	public void OnMouseExit()
+	{
+		gameObject.GetComponent<Renderer> ().material.color = Color.white;
+	}
+
+}
